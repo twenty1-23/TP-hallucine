23ba8c46d7b10e08fa7a10af0c9762776d941be8
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-17 12:19:24 -0400

    Update connexion.php
    Fonction de connexion à la BDD.

--- www/connexion.php
+++ www/connexion.php
@@ -1,8 +1,13 @@
 <?php
-    $database = new PDO("mysql:host=localhost;dbname=hallucine", "root", "Admin-01");
+    $database = connect("localhost", "hallucine", "root", "Admin-01");
     $sql = "SELECT * FROM `movies`;";
     $results = $database->query($sql);
     while ($row = $results->fetch()) {
         echo $row["id"] . $row["title"] . " " . $row["release_date"] . "<br>";
     }
+
+    function connect($host, $dbname, $login, $password){
+        return new PDO("mysql:host=".$host.";dbname=".$dbname, $login, $password);
+    }
+    
 ?>
