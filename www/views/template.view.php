ef63bf14dbc02d4314a3c3899384c258eddfdd3f
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-18 11:52:27 -0400

    Update template.view.php
    Mise en place du template.

---
+++ www/views/template.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style/styles.css" rel="stylesheet">
+    <title>Halluciné - Films</title>
+</head>
+
+<body>
+    <header>HALLUCINE</header>
+    <div id="container">
+        <div id="left">
+            <nav>
+                <ul>
+                    <li>Films</li>
+                    <li>Acteurs</li>
+                    <li>Réalisateurs</li>
+                </ul>
+            </nav>
+        </div>
+        <div id="right">
+            <select name="" id="">
+                <option value="title">Par titre</option>
+                <option value="added_date">Par date d'ajout</option>
+                <option value="release_date">Par date de sortie</option>
+            </select>
+            <div id="items">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
