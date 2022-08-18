96899b28c09dcabcc362f5592a27dd6331a05216
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-18 11:47:09 -0400

    Update HallucineModel.class.php
    HallucineModel OK.

---
+++ www/models/HallucineModel.class.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "Model.class.php";
+
+class HallucineModel extends Model{
+    private $_movies;
+
+    public function requestMovies(){
+        $_movies = array();
+        $sql = "SELECT * FROM `movies` ORDER BY title;";
+
+        $request = $this->_getDatabase("localhost", "hallucine", "root", "Admin-01")->prepare($sql);
+        $request->execute();
+        $rows = $request->fetchAll(PDO::FETCH_ASSOC);
+        $request->closeCursor();
+
+        foreach ($rows as $key => $value) {
+            $movie = new Movie($value["id"], $value["title"], $value["image_url"], $value["runtime"], $value["description"], $value["release_date"], $value["added_date"]);
+            $_movies[] = $movie;
+        }
+    }
+
+    public function getMovies(){return $this->_movies;}
+
+}
+?>
